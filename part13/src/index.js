cd731eadf728f68bb370e8aea81ea69fe62ca6a9
tornikes <t.sadghobelashvili@gmail.com>
2022-04-28 17:31:56 +0400

    fix

--- part13/src/index.js
+++ part13/src/index.js
@@ -1,15 +1,52 @@
-require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
-const express = require("express");
+require('dotenv').config();
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
+const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+// Model definition for notes
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'note',
+  }
+);
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await Note.findAll();
   res.json(notes);
+});
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
